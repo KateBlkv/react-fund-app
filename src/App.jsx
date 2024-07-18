4c74e7eb4f4a8f63649604b0b4a37e165279db7b
KateBlkv <trapam@yandex.ru>
2024-07-18 10:35:49 +0200

    counter, post list commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import ClassCounter from "./components/ClassCounter.jsx";
+import './styles/App.css'
+import PostList from "./components/PostList.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [posts, setPosts] = useState(
+        [
+            {title: 'Javascript', id: 1, body: 'Javascript - язык программирования'},
+            {title: 'Javascript 2', id: 2, body: 'Javascript - язык программирования 2'},
+            {title: 'Javascript 3', id: 3, body: 'Javascript - язык программирования 3'},
+        ]
+    )
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <div className="App">
+            <ClassCounter/>
+            <PostList posts={posts} title='Post List'/>
+        </div>
+    )
 }
 
 export default App
